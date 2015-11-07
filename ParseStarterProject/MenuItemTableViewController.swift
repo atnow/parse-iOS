1578b59ade27de2818f6fbaaf235e44125e0e9c0
bribovich <bribovich@gmail.com>
2015-11-07 12:45:21 -0500

    static menu item cells

--- ParseStarterProject/MenuItemTableViewController.swift
+++ ParseStarterProject/MenuItemTableViewController.swift
@@ -39,15 +39,16 @@ class MenuItemTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    /*
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 0
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
-    }
+        return 0
+    }*/
 
     /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
