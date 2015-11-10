e57df302d93a005cd1bfa218bc5327e19d5e2ade
bribovich <bribovich@gmail.com>
2015-11-10 13:38:00 -0500

    unaccept hopefully working - need my tasks feed

--- ParseStarterProject/ConfirmationViewController.swift
+++ ParseStarterProject/ConfirmationViewController.swift
@@ -12,6 +12,12 @@ import Parse
 class ConfirmationViewController: UIViewController {
 
     var selectedTask : PFObject?
+    var currentState = stateType.available
+    enum stateType{
+        case myTask
+        case accepted
+        case available
+    }
     
     
     @IBOutlet weak var titleLabel: UILabel!
@@ -35,14 +41,18 @@ class ConfirmationViewController: UIViewController {
             acceptButton.setTitle("Cancel", forState: UIControlState.Normal)
             acceptButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
             acceptButton.layer.borderColor = UIColor.redColor().CGColor
+            currentState = .myTask
         }
             
-        else if(selectedTask!["accepter"] != nil){
+        else if((selectedTask!["accepted"] as! Bool) == true){
             if ((selectedTask!["accepter"] as! PFUser).objectId == PFUser.currentUser()?.objectId){
-                acceptButton.setTitle("Accepted", forState: UIControlState.Normal)
+                acceptButton.titleLabel?.numberOfLines = 2
+                acceptButton.titleLabel?.textAlignment = NSTextAlignment.Center
+                acceptButton.setTitle("Accepted \r\n" + "Click to relinquish task", forState: UIControlState.Normal)
+                acceptButton.titleLabel?.adjustsFontSizeToFitWidth = true
                 acceptButton.setTitleColor(UIColor.greenColor(), forState: UIControlState.Normal)
                 acceptButton.layer.borderColor = UIColor.greenColor().CGColor
-                acceptButton.enabled = false
+                currentState = .accepted
             }
         }
         
@@ -72,10 +82,24 @@ class ConfirmationViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func acceptTask(sender: UIButton) {
+    @IBAction func buttonClick(sender: UIButton) {
         
-
+        switch currentState{
+        case .available:
+            acceptTask()
+        case .accepted:
+            unacceptTask()
+        case .myTask:
+            cancelTask()
+        default:
+            print("Invalid State")
+        }
+        
+    }
+ 
+    func acceptTask() {
         selectedTask!["accepter"] = PFUser.currentUser()
+        selectedTask!["accepted"] = true
         selectedTask!.ACL?.setPublicWriteAccess(false)
         selectedTask?.saveInBackgroundWithBlock({ (succeeded, error) -> Void in
             if(!succeeded){
@@ -95,6 +119,42 @@ class ConfirmationViewController: UIViewController {
                 self.navigationController?.popViewControllerAnimated(true)
             }
         })
+        
+    }
+    
+    func unacceptTask(){
+        let errorAlertController = UIAlertController(title: "Are you sure?", message: "Others will be able to claim this task", preferredStyle: .Alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default) { (action) in }
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.selectedTask!["accepted"] = false
+                self.selectedTask!["accepter"] = nil
+                self.selectedTask?.saveInBackground()
+                self.navigationController?.popViewControllerAnimated(true)
+            })
+        }
+        
+        errorAlertController.addAction(cancelAction)
+        errorAlertController.addAction(OKAction)
+        self.presentViewController(errorAlertController, animated: true) {}
+        
+        
+    }
+    
+    func cancelTask(){
+        let errorAlertController = UIAlertController(title: "Are you sure?", message: "This will permanently delete your task", preferredStyle: .Alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default) { (action) in }
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.selectedTask?.deleteInBackground()
+                self.navigationController?.popViewControllerAnimated(true)
+            })
+        }
+        
+        errorAlertController.addAction(cancelAction)
+        errorAlertController.addAction(OKAction)
+        self.presentViewController(errorAlertController, animated: true) {}
+        
         
         
         
