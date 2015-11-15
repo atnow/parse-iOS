ef1d2977106c13f5a521fe54ddedacc3dc755a09
Ben Holland <benholland88@gmail.com>
2015-11-15 12:24:47 -0500

    fixed temp

--- ParseStarterProject/MenuViewController.swift
+++ ParseStarterProject/MenuViewController.swift
@@ -32,7 +32,7 @@ class MenuViewController: UITableViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         let user = PFUser.currentUser()
-        profileImageView.image = user!["profilePicture"] as! UIImage
+ //       profileImageView.image = user!["profilePicture"] as! UIImage
         designHelper.formatPicture(profileImageView)
         homeLabel.textColor = designHelper.baseColor
         notificationsLabel.textColor = designHelper.baseColor
