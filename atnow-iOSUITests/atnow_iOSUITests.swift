268456b24ed89c6a30d29493353cb10952c507b3
bribovich <bribovich@gmail.com>
2016-01-20 16:28:55 -0500

    successful login test

--- atnow-iOSUITests/atnow_iOSUITests.swift
+++ atnow-iOSUITests/atnow_iOSUITests.swift
@@ -23,6 +23,7 @@ class atnow_iOSUITests: XCTestCase {
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
         
+        
         let app = XCUIApplication()
         let dartmouthEduEmailTextField = app.textFields["dartmouth.edu email"]
         dartmouthEduEmailTextField.tap()
@@ -30,8 +31,10 @@ class atnow_iOSUITests: XCTestCase {
         
         let passwordSecureTextField = app.secureTextFields["password"]
         passwordSecureTextField.tap()
+        passwordSecureTextField.tap()
         passwordSecureTextField.typeText("password")
         app.buttons["Login"].tap()
+        
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
