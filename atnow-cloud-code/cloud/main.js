393217c4d76b702d2bf9ecdf071d2990eaadcc7d
Ben Holland <benholland88@gmail.com>
2016-01-26 15:03:18 -0500

    cloud code

--- atnow-cloud-code/cloud/main.js
+++ atnow-cloud-code/cloud/main.js
@@ -2,29 +2,40 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+	response.success("Hello world!");
 });
 
 Parse.Cloud.afterSave("Notification", function(request, response){
 	var notification = request.object;
-	var owner = notification.owner
-	var query = new Parse.Query(Parse.Installation);
-	query.equalTo('user', owner);
+	console.log(notification);
+	var userQuery = new Parse.Query(Parse.User);
+	console.log(notification.get('owner'));
+	userQuery.equalTo('objectId', notification.get('owner').id);
+	userQuery.first({
+		success: function(object) {
+    		// Successfully retrieved the object.
+    		var owner = object;
+    		var query = new Parse.Query(Parse.Installation);
+    		query.equalTo('user', owner);
+    		Parse.Push.send({
+				where: query, // Set our Installation query
+				data: {
+					alert: "This is a test notification" 
+				}
+		}	, {
+			success: function() {
+				// Push was successful
+				console.log("Push was sent succesfully");
+			},
+			error: function(error) {
+				// Handle error
+				console.log("Push was sent succesfully");
 
-	Parse.Push.send({
-  	where: query, // Set our Installation query
-  	data: {
-    	alert: "This is a test notification" 
-  	}
-	}, {
-  	success: function() {
-    	// Push was successful
-			print("Push was sent succesfully");
-  	},
-  	error: function(error) {
-   	  // Handle error
-			print("Push was sent succesfully");
-
- 	 }
-	});
+			}
+		});
+    },
+    error: function(error) {
+    	alert("Error: " + error.code + " " + error.message);
+    	}
+	});	
 });
