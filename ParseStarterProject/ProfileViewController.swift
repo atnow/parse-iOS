1535f527345d46a8c36860f76fa90f3c675d0f16
Ben Holland <benholland88@gmail.com>
2016-01-28 20:57:07 -0500

    Revert "Merge branch 'master' of https://github.com/friend-jobs/parse-iOS"
    This reverts commit f371d68c43985e6ecf50f5aa0f088c4cd3d3490c, reversing
    changes made to 74e2b9f60b3f2c170556f90052634c1f35c8faf5.

--- ParseStarterProject/ProfileViewController.swift
+++ ParseStarterProject/ProfileViewController.swift
@@ -19,6 +19,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBOutlet weak var venmoSwitch: UISwitch!
     @IBOutlet weak var changePictureButton: UIButton!
     @IBOutlet weak var venmoLabel: UIImageView!
+    @IBOutlet weak var editInfoButton: UIButton!
     
     let designHelper = DesignHelper()
     let imagePicker = UIImagePickerController()
@@ -69,12 +70,14 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
 
         designHelper.formatButton(logoutButton)
         designHelper.formatPicture(profilePicture)
+        designHelper.formatButton(editInfoButton)
         
         if(!isOwnProfile){
             logoutButton.hidden = true
             venmoSwitch.hidden = true
             changePictureButton.hidden = true
             venmoLabel.hidden = true
+            editInfoButton.hidden = true
         }
         
     }
