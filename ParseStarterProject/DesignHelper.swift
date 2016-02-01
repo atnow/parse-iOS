78350531cf75266634f6adb153c98d9d25b4af20
bribovich <bribovich@gmail.com>
2016-02-01 16:27:25 -0500

    Changed expired label and changed colors

--- ParseStarterProject/DesignHelper.swift
+++ ParseStarterProject/DesignHelper.swift
@@ -11,7 +11,8 @@ import UIKit
 
 public class DesignHelper {
     
-    let baseColor = UIColor(red: 81/255, green: 190/255, blue: 248/255, alpha: 0.9)
+    let baseColor = UIColor(red: 81/255, green: 190/255, blue: 248/255, alpha: 1.0)
+    let fadeColor = UIColor(red: 81/255, green: 190/255, blue: 248/255, alpha: 0.2)
     
     func formatButton (button: UIButton){
         button.layer.cornerRadius = 8
