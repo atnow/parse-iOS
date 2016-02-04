c6c84a2bab4e60ca2434fb5c4f0e6e557c1f7d2a
Ben Holland <benholland88@gmail.com>
2016-02-04 15:52:57 -0500

    tabs

--- ParseStarterProject/RequestedTasksTableViewController.swift
+++ ParseStarterProject/RequestedTasksTableViewController.swift
@@ -19,31 +19,26 @@ class RequestedTasksTableViewController: HomeViewController {
     
     override func queryForTable() -> PFQuery {
         
-      //  let acceptorQuery = PFQuery(className: "Task")
-      //  acceptorQuery.whereKey("accepter", equalTo: PFUser.currentUser()!)
-        
-        let pvc = self.parentViewController as! RequestedTasksViewController
-        let stages = pvc.stages
-        let requesterQuery = PFQuery(className: "Task")
-        requesterQuery.whereKey("requester", equalTo: PFUser.currentUser()!)
-        
-        let query = requesterQuery
-        
-      //  let query = PFQuery.orQueryWithSubqueries([acceptorQuery, requesterQuery])
-        
-        // If no objects are loaded in memory, we look to the cache first to fill the table
-        // and then subsequently do a query against the network.
-        if self.objects!.count == 0 {
-            query.cachePolicy = .CacheThenNetwork
-        }
-        
-        query.orderByDescending("expiration")
-        return query
+//        let acceptorQuery = PFQuery(className: "Task")
+//        acceptorQuery.whereKey("accepter", equalTo: PFUser.currentUser()!)
+//        
+//        return acceptorQuery
+        return self.query!
     }
     
     override func viewDidLoad(){
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "loadTable", name: "tabSelected", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "loadList:", name: "tabSelected", object: nil)
+        let requesterQuery = PFQuery(className: "Task")
+        requesterQuery.whereKey("requester", equalTo: PFUser.currentUser()!)
         
+        self.query = requesterQuery
+        // If no objects are loaded in memory, we look to the cache first to fill the table
+        // and then subsequently do a query against the network.
+        if self.objects!.count == 0 {
+            self.query!.cachePolicy = .CacheThenNetwork
+        }
+        
+        self.query!.orderByDescending("expiration")
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell? {
@@ -56,9 +51,7 @@ class RequestedTasksTableViewController: HomeViewController {
             
         var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? PFTableViewCell
         if cell == nil {
-        cell = UITableViewCell(style: .Default, reuseIdentifier: "MyTaskCell") as? PFTableViewCell
-            
-        //cell = PFTableViewCell(style: .Subtitle, reuseIdentifier: cellIdentifier)
+            cell = UITableViewCell(style: .Default, reuseIdentifier: "MyTaskCell") as? PFTableViewCell
         }
         
         if let object = object {
@@ -101,24 +94,39 @@ class RequestedTasksTableViewController: HomeViewController {
         let userInfo = notification.userInfo! as NSDictionary
         currentTab = userInfo["number"] as! Int
         self.query = PFQuery(className: "Task")
-        //self.query?.whereKey(""
+        switch currentTab {
+            case 0:
+                self.query!.whereKey("accepted", equalTo: false)
+            case 1:
+                self.query!.whereKey("accepted", equalTo: true)
+                self.query!.whereKey("completed", equalTo: false)
+            case 2:
+                self.query!.whereKey("completed", equalTo: true)
+                self.query!.whereKey("confirmed", equalTo: false)
+            case 3:
+                self.query!.whereKey("confirmed", equalTo: true)
+            default:
+             break
+        }
+        self.loadObjects()
         
     }
-    func findRequestedTaskState(task : PFObject) -> Int{
-        if((task["confirmed"] as! Bool) == true){
-            return 3
-        }
-        else if((task["completed"] as! Bool) == true){
-            return 2
-        }
-        else if((task["accepted"] as! Bool) == true){
-            return 1
-        }
-        else{
-            return 0
-        }
-        
-    }
+    
+//    func findRequestedTaskState(task : PFObject) -> Int{
+//        if((task["confirmed"] as! Bool) == true){
+//            return 3
+//        }
+//        else if((task["completed"] as! Bool) == true){
+//            return 2
+//        }
+//        else if((task["accepted"] as! Bool) == true){
+//            return 1
+//        }
+//        else{
+//            return 0
+//        }
+//        
+//    }
     
     /*
     // MARK: - Navigation
