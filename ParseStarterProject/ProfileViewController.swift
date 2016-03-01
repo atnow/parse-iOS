643b28a49fd03289568c9eb52e229b56c138ffde
bribovich <bribovich@gmail.com>
2016-03-01 15:08:22 -0500

    added phone number to profile

--- ParseStarterProject/ProfileViewController.swift
+++ ParseStarterProject/ProfileViewController.swift
@@ -15,8 +15,10 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBOutlet weak var profilePicture: UIImageView!
     @IBOutlet weak var userFullNameLabel: UILabel!
     @IBOutlet weak var starRatings: FloatRatingView!
-    @IBOutlet weak var changePictureButton: UIButton!
     
+    @IBOutlet weak var phoneImageView: UIImageView!
+    
+    @IBOutlet weak var phoneNumberTextView: UITextView!
     let designHelper = DesignHelper()
     let imagePicker = UIImagePickerController()
     var user : PFUser? = PFUser()
@@ -72,35 +74,46 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         }
 
         designHelper.formatPicture(profilePicture)
+        profilePicture.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "changeProfilePicture"))
         
-        if(!isOwnProfile){
-            changePictureButton.hidden = true
-        } else {
+        if(isOwnProfile){
             let settingsButton = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: "displaySettings:")
             self.navigationItem.rightBarButtonItem = settingsButton
         }
-        
+        let phoneImage = UIImage(named: "phone-small")
+        phoneImageView.image = phoneImage?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        let s = self.user?["phone"] as! String
+        let s2 = String(format: "(%@) %@-%@",
+            s.substringWithRange(s.startIndex.advancedBy(1) ... s.startIndex.advancedBy(3)),
+            s.substringWithRange(s.startIndex.advancedBy(4) ... s.startIndex.advancedBy(6)),
+            s.substringWithRange(s.startIndex.advancedBy(7) ... s.startIndex.advancedBy(10))
+        )
+        phoneNumberTextView.text = s2
     }
     
     
-    @IBAction func changeProfilePicture(sender: UIButton) {
-        let profileChangeMenu = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
-        self.imagePicker.allowsEditing = true
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Default) { (action) in }
-        let takePictureAction = UIAlertAction(title: "Take new picture", style: .Default) { (action) in
-            self.imagePicker.sourceType = .Camera
-            self.presentViewController(self.imagePicker, animated: true, completion: nil)
-        }
+    func changeProfilePicture() {
+        if(isOwnProfile){
+            let profileChangeMenu = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+            self.imagePicker.allowsEditing = true
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Default) { (action) in }
+            let takePictureAction = UIAlertAction(title: "Take new picture", style: .Default) { (action) in
+                self.imagePicker.sourceType = .Camera
+                self.presentViewController(self.imagePicker, animated: true, completion: nil)
+            }
+            
+            let libraryPictureAction = UIAlertAction(title: "Choose picture", style: .Default) { (action) in
+                self.imagePicker.sourceType = .PhotoLibrary
+                self.presentViewController(self.imagePicker, animated: true, completion: nil)
+            }
+            
+            profileChangeMenu.addAction(takePictureAction)
+            profileChangeMenu.addAction(libraryPictureAction)
+            profileChangeMenu.addAction(cancelAction)
+            self.presentViewController(profileChangeMenu, animated: true, completion: nil)
         
-        let libraryPictureAction = UIAlertAction(title: "Choose picture", style: .Default) { (action) in
-            self.imagePicker.sourceType = .PhotoLibrary
-            self.presentViewController(self.imagePicker, animated: true, completion: nil)
         }
-    
-        profileChangeMenu.addAction(takePictureAction)
-        profileChangeMenu.addAction(libraryPictureAction)
-        profileChangeMenu.addAction(cancelAction)
-        self.presentViewController(profileChangeMenu, animated: true, completion: nil)
+
     }
     
     func imagePickerController(picker: UIImagePickerController,
